16237374faf3dd682d74f150021c697b377026df
TrungHieu202 <dongtrunghieu2002@gmail.com>
2023-08-27 21:07:06 +0700

    Bai tap bien so

---
+++ OBJECT/NewApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    private static void Main (string[] args)
+    {
+
+      int a =1;
+          Console.WriteLine("int: " +a);
+      long b =2;
+          Console.WriteLine("long: " +b);
+      float c =3;
+          Console.WriteLine("foat: " +c);
+      double d =4;
+          Console.WriteLine("double: " +d);
+      decimal e =5;
+          Console.WriteLine("decimal: " +e);
+      bool f =true;
+          Console.WriteLine("bool: " +f);
+      char g ='g';
+          Console.WriteLine("char: " +g);
+      string i ="dong trung hieu";
+          Console.WriteLine("string: " +i);
+
+          
+    }
+}
